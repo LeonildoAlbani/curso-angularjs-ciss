174fc263909076cbfe4c699cafc78a5a7c7f9b1f
Usuario de Treinamento 16 <leonildo.albani@gmail.com>
2016-03-15 21:00:48 -0300

    aula dia 15

--- js/controller/formCtrl.js
+++ js/controller/formCtrl.js
@@ -1,11 +1,53 @@
 app.controller("FormCtrl", FormCtrl);
 
 function FormCtrl($scope) {
-	$scope.person={
-		name: "Leonildo",
-		lastname: "Albani",
-		age: 22,
-		phone: '99990000',
-		email: "asd@asd.com"
+	$scope.indexTarefa = undefined;
+	$scope.clicouEditar = false;
+
+	$scope.tarefas=[{
+		check: true,
+		tarefa: "leite",
+		datacriacao: new Date().getTime()
+	},{
+		check: true,
+		tarefa: "bem louco",
+		datacriacao: new Date().getTime()
+	},{
+		check: false,
+		tarefa: "chip do pedro",
+		datacriacao: new Date().getTime()
+	}];
+
+	$scope.cadastrarTarefa = function ( data ) {
+		if (!!data){
+			var item = {
+				check: false,
+				tarefa: data,
+				datacriacao: new Date().getTime()
+			}
+			$scope.tarefas.push(item);
+			$scope.tarefa = undefined;
+		}
+	};
+
+	$scope.alterarTarefa = function( dataIndex ){
+		$scope.renomear = $scope.tarefas[dataIndex].tarefa;
+		$scope.indexTarefa = dataIndex;
+		$scope.clicouEditar = true;
+	}
+
+	$scope.renomearTarefa = function( data ){
+		console.log($scope.indexTarefa);
+		if (!!$scope.indexTarefa) {
+			$scope.tarefas[$scope.indexTarefa] = {
+				check: false,
+				tarefa: data,
+				datacriacao: new Date().getTime()//$scope.tarefas[$scope.indexTarefa].datacriacao
+			}
+			$scope.indexTarefa = undefined;
+			$scope.renomear = undefined;
+			$scope.clicouEditar = false;
+		}
 	}
 }
+
